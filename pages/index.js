82b7d3a9c60e4e2455ad65c65907bf2bb5d61e5e
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-12-27 18:42:03 +0100

    add bootstrap in project

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,76 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Somei - Idées de cadeaux</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Somei - Idées de cadeaux</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
+            </Head>
+            <Container fluid>
+                <header>
+                    <Row className={"my-4"}>
+                        <Col>
+                            <h1 className={styles.title}>
+                                Créez et partagez vos idées de cadeaux
+                            </h1>
+                        </Col>
+                    </Row>
+                </header>
+                <main>
+                    <Row>
+                        <Col md={6}>
+                            <h2 className={"text-center"}>
+                                Créez votre liste de cadeaux !
+                            </h2>
+                            <Row>
+                                <Col xs={{ span: 8, offset: 2 }}>
+                                    <Button variant={"dark"} size={'lg'} block>
+                                        S'inscrire
+                                    </Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={{ span: 8, offset: 2 }}>
+                                    <Button variant={"dark"} size={'lg'} block>
+                                        Se connecter
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col md={6}>
+                            <h2 className={"text-center"}>
+                                Partagez votre liste de cadeaux !
+                            </h2>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Créez et partagez vos idées de cadeaux
-        </h1>
-        <h2 className={styles.title}>
-          Créez votre liste de cadeaux pour vos amis et votre famille !
-        </h2>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+                            <Row>
+                                <Col xs={{ span: 8, offset: 2 }}>
+                                    <Button variant={"dark"} size={'lg'} block>
+                                        Découvrir des idées
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </main>
+                <footer className={styles.footer}>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{' '}
+                        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                    </a>
+                </footer>
+            </Container>
+        </>
+    )
 }
