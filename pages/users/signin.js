a8a72da8bf6eb146ce86b22159a3a9dce63f51e8
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2020-12-28 19:06:19 +0100

    add form signin - WIP

--- pages/users/signin.js
+++ pages/users/signin.js
@@ -1,4 +1,6 @@
 import SHead from "../../components/SHead";
+import Signinform from "../../components/Signinform";
+import Signupform from "../../components/Signupform";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -6,32 +8,28 @@ import Col from "react-bootstrap/Col";
 export default function Home() {
     return (
         <>
-            <SHead title={"Somei, identification"} />
+            <SHead title={"Somei, identification"}/>
 
             <Container fluid>
-<Row>
-    <Col xs={"12"}>
-        <h1 className={"text-center"}>Connectez-vous </h1>
-    </Col>
-    <Col>
-        <h2>Formulaire de connexion</h2>
+                <Row>
+                    <Col xs={"12"}>
+                        <h1 className={"text-center"}>Connectez-vous </h1>
+                    </Col>
+                    <Col>
+                        <h2>Formulaire de connexion</h2>
 
-        <form action="">
-            <label htmlFor="">Email</label>
-            <input type="text" name={"email"}/>
-        </form>
-    </Col>
-    <Col>
-        <h2>Formulaire d'inscription</h2>
+                        <Signinform />
+                    </Col>
+                    <Col>
+                        <h2>Formulaire d'inscription</h2>
 
-        <form action="">
-            <label htmlFor="">Email</label>
-            <input type="text" name={"email"}/>
-            <label htmlFor="">Mot de passe</label>
-            <input type="text" type={"password"} name={"password"}/>
-        </form>
-    </Col>
-</Row>
+                        <Signupform />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
             </Container>
         </>
     )
